e5b94c1bcf4a356b711fa53ea7785276d2580d85
mrifkyilyas <muhammadrifkyilyas@gmail.com>
2019-02-03 04:15:22 +0700

    upload

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+        function insert(num){
+            document.form.layar.value = document.form.layar.value+num
+            
+        }
+
+        function reset(){
+            
+                document.form.layar.value = ""
+            
+            
+        }
+
+        function equal(){
+            if( document.form.layar.value != ""){
+                hasil =  eval(document.form.layar.value)              
+                document.form.layar.value =hasil.toFixed(2)
+
+            }
+           
+
+            
+        }
+
+        function del(){
+            var isi = document.form.layar.value
+            document.form.layar.value = isi.substr(0,isi.length-1)
+
+
+        }
+
+        function akar(){
+            if( document.form.layar.value != ""){
+                hasil = Math.sqrt(document.form.layar.value)
+                document.form.layar.value = hasil.toFixed(2)
+
+            }
+
+        }
+
+   
