b0c2fd74d84cbb8eed9da08f76d06b2bacdb884b
mrifkyilyas <45786157+mrifkyilyas@users.noreply.github.com>
2019-02-03 04:22:38 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,8 +13,15 @@
 
         function equal(){
             if( document.form.layar.value != ""){
-                hasil =  eval(document.form.layar.value)              
-                document.form.layar.value =hasil.toFixed(2)
+                hasil =  eval(document.form.layar.value) 
+                if(hasil%1==0){
+                    document.form.layar.value =hasil
+
+                }else{
+                    document.form.layar.value =hasil.toFixed(2)
+
+                }             
+                
 
             }
            
@@ -32,7 +39,14 @@
         function akar(){
             if( document.form.layar.value != ""){
                 hasil = Math.sqrt(document.form.layar.value)
-                document.form.layar.value = hasil.toFixed(2)
+                if(hasil%1==0){
+                    document.form.layar.value = hasil
+
+                }else{
+                    document.form.layar.value = hasil.toFixed(2)
+
+                }
+                
 
             }
 
